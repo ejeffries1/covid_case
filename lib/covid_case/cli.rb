48a03e4fb0a5520cc6aa86928ea8363b2ed39af7
evan <ejeffries14@gmail.com>
2021-08-24 02:37:06 -0400

    cli trial

--- lib/covid_case/cli.rb
+++ lib/covid_case/cli.rb
@@ -1,13 +1,12 @@
 class CovidCase::CLI
 
-    #BASE_PATH = "https://www.nytimes.com/interactive/2021/us/covid-cases.html"
 
     def call
         puts "Welcome to the Covid-Case CLI"
-        puts "Please make a selection"
+        puts "Please type the first letter of the state you would like to view or type list to view all states available."
         get_states
-        list_states
-        get_user_state
+        #list_states
+        get_user_input
         #binding.pry
         #get_case(state)
     end
@@ -16,30 +15,50 @@ class CovidCase::CLI
         @place = CovidCase::States.all
     end
 
+    def get_user_input
+        @input = gets.strip
+        if @input == "list"
+            list_states
+            get_user_input
+        elsif @input == int.to_i > @place.count
+            get_user_state
+        else state.name.start_with?(input)
+            @place.select.with_index(1) do |state, index|
+                puts "#{index}. #{state.name}"
+            end
+        end
+    end
+
     def list_states
         @place.each.with_index(1) do |state, index|
-            puts "#{index}.#{state.name}"
+                puts "#{index}. #{state.name}"
         end
     end
 
     def get_user_state
-        chosen_state = gets.strip.to_i
-        show_state_details(chosen_state)
+        #@input = gets.strip.to_i
+        show_state_details(input)
     end
 
-    def valid_input(input)
-        @index = input.to_i
-        @index <= @place.count && @index > 0
-    end
+    #def valid_input(input)
+    #    @index = input.to_i
+    #    @index <= @place.count && @index > 0
+    #end
 
     def show_state_details(chosen_state)
         location = @place[chosen_state-1]
         CovidCase::States.get_state_stat(location.state_url)
         CovidCase::Covid_info.all.each do |stat|
-            #binding.pry
-            puts "#{stat.death[2]}"
-            puts "#{stat.cases}"
-            puts "#{stat.vaccinated}"
+            puts "Here are your results for #{location.name}"
+            puts "#########################################"
+            puts "Death ....................... #{stat.death[2]}"
+            puts "Cases ....................... #{stat.cases}"
+            puts "Vaccination ................. #{stat.vaccinated}"
         end
+            puts "#########################################"
+            puts "Please make another selection"
+            puts "If you wish to exit, press n"
+            input = gets.strip
+            call unless input == "n"
     end
 end
